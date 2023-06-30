58e10bbaaf1d1b0f57fc38ab63bb5f654e27cbb0
Sbordi <110256170+Sbord1@users.noreply.github.com>
2023-06-30 23:26:30 +0200

    Update calcio.php

--- codice/calcio.php
+++ codice/calcio.php
@@ -5,26 +5,40 @@
 	<head>
 		<title> Calcio </title>
 		<style>
-  		 td.heading {
-  			border: 1px solid black;
-  			padding: 4px 6px;
-			}
+		tr {
+			background-color: rgb(228, 240, 245);
+			height: 30px;}
+  		 td.head{
+  		 	background-color: #FDEBD0;
+  		 	}
 
 		.tablecenter{
 					padding: 10px;
-					border-spacing: 10px 0;
 					margin-right: auto;
-					margin-left: auto;}
+					margin-left: auto;
+					width: 75%;
+					
+					}
+		.link-button {
+  					background: none;
+  					border: none;
+  					color: black;
+  					cursor: pointer;
+ 				 	text-decoration: underline;
+  					font-size: 1em;
+  					font-family: serif;
+					}
 		</style>
 	</head>
 
 	<body style= "background-color: #34495E" >
+	<?php require("./menuConSwitch.php"); ?>
 	
-		
+	<h4 style=" color: white; margin-left:36%"> Clicca sulla quota che desideri per piazzare la scommessa </h4>
     <table class="tablecenter">
     	<tbody>
 <?php 
-
+	
 
 $xmlString = "";
 foreach ( file("calcio.xml") as $node ) {
@@ -43,25 +57,25 @@ foreach ( file("calcio.xml") as $node ) {
     $lunghezza = $calcio->length;
    	
    	// se questa pagina e' stata chiamata con "next=qualchecosa" nella
-	// uri di chiamata, vuol dire che avevamo gia' stampato qualcosa e riprendiamo da lì;
+	// uri di chiamata, vuol dire che avevamo gia' stampato qualcosa e riprendiamo da lÃ¬;
 
 
 
 	$elenco = "<tr> 
-					<td> Ora di inizio </td>
-					<td> Partita </td>
-					<td> 1 </td>
-					<td> 2 </td>
-					<td> X </td>
-					<td> Under 2.5 </td>
-					<td> Over 2.5 </td>
-					<td> GG </td>
-					<td> NG </td> </tr>\n";
+					<td class=\"head\"> Ora di inizio </td>
+					<td class=\"head\"> Partita </td>
+					<td class=\"head\"> 1 </td>
+					<td class=\"head\"> 2 </td>
+					<td class=\"head\"> X </td>
+					<td class=\"head\"> Under 2.5 </td>
+					<td class=\"head\"> Over 2.5 </td>
+					<td class=\"head\"> GG </td>
+					<td class=\"head\"> NG </td> </tr>\n";
 
 	//ciclo per ottenere info su tutti i film di una stessa categoria
    for ($i=0; $i<$lunghezza; $i++) {
 	
-		$scommessa = $calcio->item($i); //è uno dei record
+		$scommessa = $calcio->item($i); //Ã¨ uno dei record
 	
 		$id = $scommessa->firstChild; //id primo child
 		$idNumber = $id->textContent;
@@ -115,26 +129,66 @@ foreach ( file("calcio.xml") as $node ) {
 		$puntiSquadraTrasferta = $risultato->lastChild;
 		$puntiSquadraTrasfertaValue = $puntiSquadraTrasfertaValue->textContent;
 	
-		
-		
-	
+
+// cliccando sulla quota, mi rimanda alla pagina piazzaScommessa.php dove so quale quota ho cliccato e di quale squadra
 		$elenco.="\n<tr>
 						<td> $oraInizioValue </td>
-						 <td>  $squadraCasaValue - $squadraTrasfertaValue </td>
-						 <td> $quota1Value </td>
-						 <td> $quotaxValue </td>
-						 <td> $quota2Value </td>
-						 <td> $quotaUnderValue </td>
-						 <td> $quotaOverValue </td>
-						 <td> $quotaGGValue </td>
-						 <td> $quotaNGValue </td> </tr>\n";
+						<td> $squadraCasaValue - $squadraTrasfertaValue </td>
+						 <td> 
+						 		<form method=\"post\" action=\"piazzaScommessa.php\"> 
+						  		<button type=\"submit\" name=\"submit\" value=\"submit\" class=\"link-button\"> $quota1Value </button>
+						  		<input type=\"hidden\" name=\"squadraCasa\" value=\"$squadraCasaValue\">
+						 		<input type=\"hidden\" name=\"squadraTrasferta\" value=\"$squadraTrasfertaValue\">
+						 		<input type=\"hidden\" name=\"quota\" value=\"$quota1Value\"> </form>
+						</td>
+						<td> 
+								<form method=\"post\" action=\"piazzaScommessa.php\"> 
+								<button type=\"submit\" name=\"submit\" value=\"submit\" class=\"link-button\"> $quotaxValue </button>
+								<input type=\"hidden\" name=\"squadraCasa\" value=\"$squadraCasaValue\">
+								<input type=\"hidden\" name=\"squadraTrasferta\" value=\"$squadraTrasfertaValue\">
+								<input type=\"hidden\" name=\"quota\" value=\"$quotaxValue\"> </form> 
+						 </td>
+						 <td>
+								<form method=\"post\" action=\"piazzaScommessa.php\"> 
+								<button type=\"submit\" name=\"submit\" value=\"submit\" class=\"link-button\"> $quota2Value </button>
+								<input type=\"hidden\" name=\"squadraCasa\" value=\"$squadraCasaValue\">
+								<input type=\"hidden\" name=\"squadraTrasferta\" value=\"$squadraTrasfertaValue\">
+								<input type=\"hidden\" name=\"quota\" value=\"$quota2Value\"> </form>
+						 </td>
+						 <td>
+								<form method=\"post\" action=\"piazzaScommessa.php\"> 
+								<button type=\"submit\" name=\"submit\" value=\"submit\" class=\"link-button\">  $quotaUnderValue </button>
+								<input type=\"hidden\" name=\"squadraCasa\" value=\"$squadraCasaValue\">
+								<input type=\"hidden\" name=\"squadraTrasferta\" value=\"$squadraTrasfertaValue\">
+								<input type=\"hidden\" name=\"quota\" value=\" $quotaUnderValue\"> </form>
+						</td>
+						<td> 
+						 		<form method=\"post\" action=\"piazzaScommessa.php\"> 
+								<button type=\"submit\" name=\"submit\" value=\"submit\" class=\"link-button\">  $quotaOverValue  </button>
+								<input type=\"hidden\" name=\"squadraCasa\" value=\"$squadraCasaValue\">
+								<input type=\"hidden\" name=\"squadraTrasferta\" value=\"$squadraTrasfertaValue\">
+								<input type=\"hidden\" name=\"quota\" value=\" $quotaOverValue \"> </form>
+						</td>
+						<td>
+						 		<form method=\"post\" action=\"piazzaScommessa.php\"> 
+								<button type=\"submit\" name=\"submit\" value=\"submit\" class=\"link-button\">  $quotaGGValue  </button>
+								<input type=\"hidden\" name=\"squadraCasa\" value=\"$squadraCasaValue\">
+								<input type=\"hidden\" name=\"squadraTrasferta\" value=\"$squadraTrasfertaValue\">
+								<input type=\"hidden\" name=\"quota\" value=\" $quotaGGValue \"> </form> 
+						</td>
+						<td>
+								<form method=\"post\" action=\"piazzaScommessa.php\"> 
+								<button type=\"submit\" name=\"submit\" value=\"submit\" class=\"link-button\">  $quotaNGValue  </button>
+								<input type=\"hidden\" name=\"squadraCasa\" value=\"$squadraCasaValue\">
+								<input type=\"hidden\" name=\"squadraTrasferta\" value=\"$squadraTrasfertaValue\">
+								<input type=\"hidden\" name=\"quota\" value=\" $quotaNGValue \"> </form>
+						</td> </tr>\n";
 	
 			
 }
 
 	echo "$elenco";
 	echo "</tbody>\n</table>";
-    	
 ?> 	
 	</body>
 </html>
