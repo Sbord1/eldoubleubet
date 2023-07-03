9a4e9858c29bc303cd524ffe1de46d011d77c9e5
Sbordi <110256170+Sbord1@users.noreply.github.com>
2023-07-03 21:49:34 +0200

    Update listaUtenti.php

--- codice/listaUtenti.php
+++ codice/listaUtenti.php
@@ -3,6 +3,37 @@
 	require_once("./connection.php");
 	session_start();
 	
+	if (isset($_POST['attiva']) ||  isset($_POST['disattiva'])) {
+	
+	//apro file della chat, questo per inviare la comunicazione all'utente
+		$xmlString2="";
+		# Name of the output file
+		$xml_file_name2 = 'fileXML/chat/chat.xml';
+	
+		foreach ( file("$xml_file_name2") as $node ) {
+		$xmlString2 .= trim($node);
+	}
+		
+		$doc2 = new DOMdocument();
+		$doc2->loadXML($xmlString2);
+	
+
+	   if (!$doc2->loadXML($xmlString2)) {
+			die ("Error mentre si andava parsando il documento\n");
+	}
+		$chat = $doc2->documentElement; //root
+		$lunghezza2 = $chat->childNodes->length;  //lunghezza corrisponde a quante richieste ci sono
+		
+		
+		
+		//Ottengo l'id dell'ultimo nodo (chat.xml), questo perchè il nuovo nodo deve avere id+1
+		$last_id = $doc2->getElementsByTagName("id")[$lunghezza2-1]->nodeValue;
+		$last_id = $last_id + 1;
+		
+		$date = date("Y-m-d");
+    	$time = date("H:i:s");
+		
+		
 	//se cliccato bottone per attivare account, attivalo e imposta credito a 20
 	if (isset($_POST['attiva'])){
 		$sqlQuery = "UPDATE $DBuser_table
@@ -14,6 +45,26 @@
    			printf("Oops! La query inviata non ha avuto successo!\n");
 			exit();
 		}
+		
+		//invio comunicazione se account attivato (non ha senso inviarla quando è disattivo perche non può accedere al sito)
+		$newRecord = $doc2->createElement("message");
+		$newId = $doc2->createElement("id", $last_id);
+		$newSender = $doc2->createElement("sender", $_SESSION['userName']);
+		$newReceiver = $doc2->createElement("receiver", $_POST['username']);
+		$newText = $doc2->createElement("text","Account Attivato");
+		$newData = $doc2->createElement("data", $date);
+		$newOra = $doc2->createElement("ora", $time);
+
+		$newRecord->appendChild($newId);
+		$newRecord->appendChild($newSender);
+		$newRecord->appendChild($newReceiver);
+		$newRecord->appendChild($newText);
+		$newRecord->appendChild($newData);
+		$newRecord->appendChild($newOra);
+
+		$chat->appendChild($newRecord);
+		$doc2->save("$xml_file_name2");
+		
 	}
 	//se cliccato bottone per disattivare account, disattivalo e imposta credito a 0
 		if (isset($_POST['disattiva'])){
@@ -27,6 +78,7 @@
 			exit();
 		}
 	}
+}
 
 // Selezioniamo tutti gli utenti dalla tabella DBuser
 	$sqlQuery = "SELECT * FROM $DBuser_table";
@@ -156,6 +208,7 @@ while ($row = mysqli_fetch_array($resultQ)) {
 	echo "<td>".$row['account']."</td>";
 	echo "<form method=\"post\" action=\"\">";
 	echo "<input type=\"hidden\" name=\"id\" value=\"$row[id]\">";
+	echo "<input type=\"hidden\" name=\"username\" value=\"$row[username]\">";
 	if ($row['account']=='disattivo'){
 		echo "<td> <button type=\"submit\" name=\"attiva\" value=\"submit\" class=\"link-button\">  Attiva account</button> </td>";
 	}
