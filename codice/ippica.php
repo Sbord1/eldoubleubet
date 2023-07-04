63da113015a7fed32672204dc7eb6bfb2506d6ad
Sbordi <110256170+Sbord1@users.noreply.github.com>
2023-07-04 16:42:04 +0200

    Update ippica.php

--- codice/ippica.php
+++ codice/ippica.php
@@ -3,6 +3,12 @@
 	error_reporting(E_ALL);
 
 	session_start();
+	
+	//se sei gestore appare un buttone per eliminare le scommesse
+	$eliminato_button="";
+	if ($_SESSION['tipologia']=="gestore"){
+		$eliminato_button="Elimina";}
+		
 ?>
 
 <?xml version="1.0" encoding="ISO-8859-1"?>
@@ -213,12 +219,70 @@ foreach ( file("fileXML/scommesseDisponibili/ippica.xml") as $node ) {
 
     $ippica = $doc-> documentElement-> childNodes;
     $lunghezza = $ippica->length;
+    
+    //se premuto pulsante elimina, cambio attributo alla scommessa
+   	if (isset($_POST['elimina'])){
+   	
+   		$scommesse = $doc->getElementsByTagName('scommessa'); //ottengo lista di scommesse
+   		
+   		foreach ($scommesse as $s){
+   			if($s->childNodes[0]->nodeValue == $_POST['idPartita']){
+   				$s->setAttribute("eliminato","1");
+   			}
+   			
+		} //fine for each
+		
+					
+		$doc->save("fileXML/scommesseDisponibili/ippica.xml");
+	
+		/////////////////////////////////////////////////////////////////
+		//ora devo cancellare scommessa nelle scommesse degli utenti
+		$xmlString2 = "";
+		foreach ( file("fileXML/scommesseUtenti/scommesseIppica.xml") as $node ) {
+			$xmlString2 .= trim($node);
+		}
+
+		$doc2 = new DOMdocument();
+		$doc2->loadXML($xmlString2);
+	
+		if (!$doc->loadXML($xmlString2)) {
+			die ("Error mentre si andava parsando il documento\n");
+		}
+		
+		$scommesseUtenti = $doc2->getElementsByTagName("scommessa"); //ottengo lista di scommesse utente
+		foreach ($scommesseUtenti as $su){
+			if($su->childNodes[0]->nodeValue == $_POST['idPartita']){
+			$nomeScommettitore = $su->childNodes[2]->nodeValue;
+			$puntataScommettitore = $su->childNodes[4]->nodeValue;
+			$su->setAttribute("eliminato","1");
+			}
+		}//fine for each
+		
+		require_once("./connection.php");
+					
+					//aggiorno credito utente
+  					$sqlQuery = "UPDATE $DBuser_table
+					set credito= credito + $puntataScommettitore
+					where username = \"$nomeScommettitore\"";
+					
+					$resultQ = mysqli_query($mysqliConnection, $sqlQuery);
+					
+					if (!$resultQ) {
+   						printf("Oops! La query inviata non ha avuto successo!\n");
+						exit();
+					}
+					
+		$doc2->save("fileXML/scommesseUtenti/scommesseIppica.xml");	
+	}	//fine if elimina
+	
+	
 
 	//ciclo per ottenere info su tutte le scommesse di calcio
    for ($i=0; $i<$lunghezza; $i++) {
 	
 		$scommessa = $ippica->item($i); 
-	
+		$eliminato = $scommessa->getAttribute("eliminato");
+		
 		$id = $scommessa->firstChild; 
 		$idNumber = $id->textContent;
 
@@ -367,7 +431,8 @@ foreach ( file("fileXML/scommesseDisponibili/ippica.xml") as $node ) {
         $risultatoTerzoValue = $risultatoTerzo->textContent;
 
         $anno_mese_giorno = trim($annoValue)."-".trim($meseValue)."-".trim($giornoValue);
-
+        
+		if ($eliminato == "0"){
         // Costruiamo i titoli delle colonne della tabella che conterra' le scommesse di ippica estratte da ippica.xml
         $elenco = "
         <table class=\"tablecenter\">
@@ -425,11 +490,22 @@ foreach ( file("fileXML/scommesseDisponibili/ippica.xml") as $node ) {
         </td>
         <td rowspan=\"8\">$risultatoPrimoValue - $risultatoSecondoValue - $risultatoTerzoValue</td>
         <td rowspan=\"8\">$idNumber</td>
-        <td rowspan=\"8\">$distanzaValue"."m</td>
+        <td rowspan=\"8\">$distanzaValue"."m</td>";
+		$current_date = date("Y-m-d");
+		$current_time = date("H:i:s");
+		//se evento non svolto mostro il bottone per eliminare
+		if ( ($current_date < $anno_mese_giorno) || (($current_date == $anno_mese_giorno) && ($current_time < $oraFineValue)) ) {
+							
+		$elenco.="<td rowspan=\"8\">
+							<form method=\"post\" action=\"\"> 
+							<button type=\"submit\" name=\"elimina\" value=\"elimina\" class=\"link-button\">  $eliminato_button  </button>
+							<input type=\"hidden\" name=\"idPartita\" value=\"$idNumber\"> </form>
+					</td>";
+					}
+		$elenco.="</tr>\n";
+		
 
-        </tr>\n
-
-        <tr>
+       $elenco.=" <tr>
             <td></td>
             <td></td>
             <td></td>
@@ -700,7 +776,7 @@ foreach ( file("fileXML/scommesseDisponibili/ippica.xml") as $node ) {
         ";
 
         echo "$elenco";
-
+}
 }
 
 
